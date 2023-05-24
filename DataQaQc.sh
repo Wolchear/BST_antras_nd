b28ac7cec64c3f45b16467732b9841262c679d2c
Wolchear <npavliukovec@gmail.com>
2023-05-25 01:59:20 +0300

    Comments fastqc

--- DataQaQc.sh
+++ DataQaQc.sh
@@ -9,6 +9,29 @@ threads=6
 #fi
 #fastqc -t $threads ../../inputs/* -o ../../outputs/raw_data/
 
+#Komentarai
+# ERR204044
+# Vienintelė problema buvo su `Per tile sequence quality`
+# Kaip supratau problema gali atsirasti
+# dėl `flowcell` nešvarumo it t.t. 
+#(https://www.bioinformatics.babraham.ac.uk/projects/fastqc/Help/3%20Analysis%20Modules/12%20Per%20Tile%20Sequence%20Quality.html)
+
+#SRR15131330
+# Buvo fail`as tik su `Sequence Duplication Levels`
+# Su `overrpresented sequences` ir adapteriais viskas yra ok
+# Tikriausiai tai tsg sekos specifiškumas
+# https://www.bioinformatics.babraham.ac.uk/projects/fastqc/Help/3%20Analysis%20Modules/8%20Duplicate%20Sequences.html
+
+#SRR18214264
+# Failas `Per base sequence content`.
+# Jei pažiūrėti į `Sequence Length Distribution`
+# Matosi, kad SRR18214264 sampl'as turi šiek tiek read`ų
+# kurie yra trumpesni už 150-152 (SRR15131330 visi read`ai
+# yra 150-152 bp o ERR204044 99-101 ), tai gal tas "failas" susiję su tais "short read`ais"
+# https://www.bioinformatics.babraham.ac.uk/projects/fastqc/Help/3%20Analysis%20Modules/4%20Per%20Base%20Sequence%20Content.html
+# Šaltinyje parašyta, kad toks rezultatas gali atsirasti dėl nesubalansoto fragmentavimo (Biased fragementation)
+# Jeigu viska supratau, tai ir yra pagrindinė priežastis
+
 #2)Trim galore
 if [ ! -d ../../inputs/trimmed ]; then
 mkdir -p ../../inputs/trimmed
@@ -24,6 +47,8 @@ trim_galore -paired $R1 $R2 --fastqc -o ../../inputs/trimmed/ --length 20 -q 25 
 done
 echo "All samples are trimmed"
 
+# ERR204044
+#
 
 #3) MultiQc
 
